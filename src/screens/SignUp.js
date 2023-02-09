610440b18784aa8460d14b68aa51beac895f9784
Emmanuella Albuquerque <emmanuellaf.albuquerque@gmail.com>
2023-02-09 16:15:41 -0300

    Adding loading until removing The Movie DB info panel with injected javaScript
    - hidden webview with opacity property

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,15 +1,31 @@
+import { useState } from 'react';
+import { Text, Center,  } from 'native-base';
 import { WebView } from 'react-native-webview';
+import { ActivityIndicator } from 'react-native';
 
 export function SignUp() {
 
+  const [loading, setLoading] = useState(true);
   const jsCode = `document.querySelector('.settings_panel').style.display = 'none';`;
 
   return (
-    <WebView
+    <>
+      {
+        loading && 
+        <Center flexGrow={3}>
+          <ActivityIndicator size={'large'} />
+        </Center>
+      }
+      <WebView
       source={{ uri: 'https://www.themoviedb.org/signup' }}
       originWhitelist={['*']}
       javaScriptEnabledAndroid={true}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
       injectedJavaScript={jsCode}
-    />
+      onLoad={() => setLoading(false)}
+      opacity={loading ? 0 : 1} // hidden webview
+      />
+    </>
   );
 }
